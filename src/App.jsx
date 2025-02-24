559aaf9bed6e07b6db1f57a2d8341182bf723fd2
ritesh <riteshramteke199@gmail.com>
2025-02-24 17:58:29 +0530

    added push filter

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects</h2>
+      <h2>Complete course with 2 projects sushant</h2>
     </>
   )
 }
